beafe671109033acbccea553bd023a21a692eab7
Akemi Noriega <ake.sudestada@gmail.com>
2023-05-17 15:10:14 +0200

    v2arregloSearch.Page

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -7,22 +7,34 @@ export const SearchPage = () => {
 	const location = useLocation();
 
 	const { globalPokemons } = useContext(PokemonContext);
+	let filteredPokemons = null
 
-	const filteredPokemons = globalPokemons.filter(pokemon =>
-		pokemon.name.includes(location.state.toLowerCase())
-	);
+  if(location.state!=null){
+   filteredPokemons = globalPokemons.filter(pokemon => pokemon.name.includes(location.state.toLowerCase()))
+  console.log("entra1")
+  }
 
 	return (
-		<div className='container'>
-			<p className='p-search'>
-				Se encontraron <span>{filteredPokemons.length}</span>{' '}
-				resultados:
-			</p>
-			<div className='card-list-pokemon container'>
-				{filteredPokemons.map(pokemon => (
-					<CardPokemon pokemon={pokemon} key={pokemon.id} />
-				))}
-			</div>
-		</div>
-	);
-};
+
+		location.state ? (
+		  <div className="container">
+		  <p className="p-search">
+			Se encontraron <span>{filteredPokemons.length}</span>{' '}resultados:
+		  </p>
+		  <div className="card-list-pokemon container">
+			{filteredPokemons.map(pokemon => <CardPokemon pokemon={pokemon} key={pokemon.id}/>)}
+		  </div>
+		 </div>
+		 
+		 ):(
+		  
+		  <p className="p-search">
+			Se encontraron <span>0</span>{' '}resultados:
+		  </p>
+		 
+		
+	   )
+		
+	   
+	  )
+	}
